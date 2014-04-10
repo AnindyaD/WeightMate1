be0a07db07c6cb1b5f060086b7a437dc14e86b09
justinli1 <justin.li.jl@gmail.com>
2014-04-09 20:49:28 -0400

    few more fixes

--- WeightMate/src/com/example/weightmate/DailyPlannerActivity.java
+++ WeightMate/src/com/example/weightmate/DailyPlannerActivity.java
@@ -28,13 +28,13 @@ public class DailyPlannerActivity extends ActionBarActivity {
 
 	}
 
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.daily_planner, menu);
-//		return true;
-//	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.daily_planner, menu);
+		return true;
+	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
